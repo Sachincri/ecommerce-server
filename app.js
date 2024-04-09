d0107926472439fb5dbf0b4669a66ad0d20353b0
your_username <ss9047858@gmail.com>
2024-04-09 15:02:15 +0530

    Changes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(
   cors({
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    origin: process.env.FRONTEND_URI_1,
+    origin: [process.env.FRONTEND_URI_1, process.env.FRONTEND_URI_2],
   })
 );
 
